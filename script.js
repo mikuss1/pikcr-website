458430426d18410ab529ecf7e88cc32679b20acd
mikuss1 <miikuss.ty@gmail.com>
2024-01-13 11:39:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+  }
+
+  function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+  }
